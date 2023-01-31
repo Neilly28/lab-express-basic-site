0bfd02642fdec7240747928437b382d97c6b004d
Neilly <neilpilarca@gmail.com>
2023-01-31 14:44:46 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// require express
+const express = require("express");
+
+// capture express method in app variable
+const app = express();
+
+// give access to assets
+app.use(express.static(__dirname + "/public"));
+
+// start the express server
+app.listen(3000, () => {
+  console.log("listening from port 3000!");
+});
+
+// creating routes
+app.get("/", (req, res) => {
+  //   res.send("hello from the homepadadsge!");
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+// creating routes
+app.get("/about", (req, res) => {
+  //   res.send("hello from the about page!");
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+// creating routes
+app.get("/works", (req, res) => {
+  res.send("hello from the works page!");
+});
+
+// creating routes
+app.get("/gallery", (req, res) => {
+  res.send("hello from the gallery page!");
+});
+
+// creating routes
+app.get("*", (req, res) => {
+  res.send("I don't know that route!");
+});
